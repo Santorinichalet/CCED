98b40180f4c17854768d65664e94a61e5cbbba64
Santorinichalet <abojamal41@gmail.com>
2026-01-14 08:02:22 +0400

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// Firebase configuration (no ES module exports; compatible with GitHub Pages <script> tags)
+window.firebaseConfig = {
+  apiKey: "AIzaSyA8ovxv3cGURJ_6XSbf3ICoMOPHDHLM9Sc",
+  authDomain: "cced-74ca6.firebaseapp.com",
+  projectId: "cced-74ca6",
+  storageBucket: "cced-74ca6.firebasestorage.app",
+  messagingSenderId: "940577322588",
+  appId: "1:940577322588:web:86030574b4621973e55899",
+  measurementId: "G-WCF55SSPKY"
+};
